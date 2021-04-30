5c8168df6922bb3e117fbd4fd307fc8c2fdef90a
happysunf1ower <74223562+happysunf1ower@users.noreply.github.com>
2021-04-29 19:13:38 -0700

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,59 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play()
+  {
+    background("teal");
+    
+    question.hide();
+
+    contestant.getContestantInfo();
+    
+    if(allContestants !== undefined)
+    {
+      fill("blue");
+      textSize(20);
+      text("NOTE: Contestant who answered correct are highlighted in green color!", 130, 230);
+     
+    }
+
+    for(var contestant in allContestants){
+      var correctAns = "2";
+      index = index + 1;
+      if(correctAns === allContestants[plr].answer)
+      {
+        fill("green");
+      } else{
+        fill("red");
+      }
+    }
+  }
+
+}
